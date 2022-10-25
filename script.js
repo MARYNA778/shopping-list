77be69d938ec099f4dd6ef939f8cc893735ec570
MARYNA778 <ruliova778@mail.com>
2022-10-25 10:32:26 +0300

    Update script.js
    создали элемент списка

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+const container = document.querySelector('.items'); //нашли весь контейнер 
+const input = document.querySelector("input");
+// Вешаем обработчик нажатия клавиши
+input.addEventListener("keydown", function(event) {
+    // Если нажата клавиша Enter
+    if (event.key === "Enter") {
+        const shopping = document.createElement('li'); // создаём элемент списка <li> 
+        shopping.addEventListener('click', function() {
+            shopping.classList.toggle('done');
+
+        });
+
+        container.append(shopping); // длбавляем его в контейнер
+        shopping.textContent = input.value;
+        input.value = ''; // очистка инпута    
+
+
+    };
+});
+
+
 /* Новые элементы должны добавляться в список по нажатию на Enter */
 
 /* Пустые элементы не должны добавляться */
